89278ab14e81f2478ec08426b210dd62431ab829
PJ82 <project82@mail.ru>
2025-07-04 15:59:42 +0500

    .

--- include/esp32_c3_utils/temp_sensor.h
+++ include/esp32_c3_utils/temp_sensor.h
@@ -21,19 +21,19 @@ namespace esp32_c3_utils
      * @param dacOffset Смещение DAC (TSENS_DAC_L0 = -10, L4 = +10)
      * @return esp_err_t Код ошибки ESP_OK при успехе
      */
-    esp_err_t initTempSensor(temp_sensor_dac_offset_t dacOffset = TSENS_DAC_DEFAULT);
+    esp_err_t initTempSensor(temp_sensor_dac_offset_t dacOffset = TSENS_DAC_DEFAULT) noexcept;
 
     /**
      * @brief Получить температуру чипа
      * @param[out] temperature Ссылка для сохранения температуры
      * @return esp_err_t Код ошибки ESP_OK при успехе
      */
-    esp_err_t getChipTemperature(float& temperature);
+    esp_err_t getChipTemperature(float& temperature) noexcept;
 
     /**
      * @brief Остановить датчик температуры (для экономии энергии)
      */
-    void deinitTempSensor();
+    void deinitTempSensor() noexcept;
 } // namespace esp32_c3_utils
 
 #endif //ESP32_C3_UTILS_TEMP_SENSOR_H
