c45ddcadf53924ac21a83095733f1cbca1e7a679
PJ82 <project82@mail.ru>
2025-07-24 21:48:39 +0500

    .

---
+++ include/esp32_c3_utils/type_utils.h
@@ -0,0 +1,44 @@
+#ifndef ESP32_C3_TYPE_UTILS_H
+#define ESP32_C3_TYPE_UTILS_H
+
+#include <array>
+#include <cstdio>
+#include <cstring>
+#include <esp_log.h>
+
+namespace esp32_c3::utils
+{
+    /**
+     * @brief Генерация тега для логов на основе типа
+     * @tparam T Тип для которого генерируется тег
+     * @return constexpr std::array<char, N> с текстом тега
+     */
+    template <typename T>
+    constexpr auto generateTag(const char* class_name)
+    {
+        constexpr auto type_name =
+#ifdef _MSC_VER
+            __FUNCSIG__;
+#else
+            __PRETTY_FUNCTION__;
+#endif
+
+        // Ищем начало типа в строке
+        const char* type_start = strstr(type_name, "T = ") + 4;
+        const char* type_end = strchr(type_start, ';');
+        if (!type_end) type_end = strchr(type_start, ']');
+        if (!type_end) type_end = type_start + strlen(type_start);
+
+        // Вычисляем длину
+        const size_t type_len = type_end - type_start;
+
+        // Создаем массив для хранения
+        std::array<char, 64> result{};
+        snprintf(result.data(), result.size(), "%s<%.*s>",
+                 class_name, static_cast<int>(type_len), type_start);
+
+        return result;
+    }
+} // namespace esp32_c3::utils
+
+#endif //ESP32_C3_TYPE_UTILS_H
